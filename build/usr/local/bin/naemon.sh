47740c31e9f2d9e83230ab584f65002f1a272eab
Bryan stewart <bryan.stewart.2015@gmail.com>
2024-10-15 20:17:03 -0500

    added test check

--- build/usr/local/bin/naemon.sh
+++ build/usr/local/bin/naemon.sh
@@ -6,3 +6,6 @@ yum install -y yum-utils oracle-epel-release-el9
 yum install -y naemon
 
 mkdir -p /etc/naemon/conf.d/{instances,network,terraform}
+mkdir -p /usr/lib64/nagios/plugins
+chown -R naemon:naemon /usr/lib64/nagios/plugins
+chmod 755 /usr/lib64/nagios/plugins
